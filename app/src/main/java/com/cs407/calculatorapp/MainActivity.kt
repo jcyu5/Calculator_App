3bea2d4fb209803c9a91fa7ccb99a5aa1f18664d
Jonathan Yu <jcyu5@wisc.edu>
2024-09-19 16:20:38 -0500

    Calculator App Basic Functionality Implemented

--- app/src/main/java/com/cs407/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/cs407/calculatorapp/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.cs407.calculatorapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,72 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val editText1 = findViewById<EditText>(R.id.editText1)
+        val editText2 = findViewById<EditText>(R.id.editText2)
+
+        val additionButton = findViewById<Button>(R.id.additionButton)
+        additionButton.setOnClickListener{
+            try {
+                val input1 = editText1.text.toString().toInt()
+                val input2 = editText2.text.toString().toInt()
+                val result = input1 + input2
+                val intent = Intent(this, CalculatorActivity::class.java)
+                intent.putExtra("RESULT", result.toString())
+                startActivity(intent)
+            } catch(e: NumberFormatException){
+                val error = Toast.makeText(this, "Please Enter Valid Integers", Toast.LENGTH_SHORT)
+                error.show()
+            }
+        }
+
+        val subtractionButton = findViewById<Button>(R.id.subtractionButton)
+        subtractionButton.setOnClickListener{
+            try {
+                val input1 = editText1.text.toString().toInt()
+                val input2 = editText2.text.toString().toInt()
+                val result = input1 - input2
+                val intent = Intent(this, CalculatorActivity::class.java)
+                intent.putExtra("RESULT", result.toString())
+                startActivity(intent)
+            } catch(e: NumberFormatException){
+                val error = Toast.makeText(this, "Please Enter Valid Integers", Toast.LENGTH_SHORT)
+                error.show()
+            }
+        }
+
+        val multiplicationButton = findViewById<Button>(R.id.multiplicationButton)
+        multiplicationButton.setOnClickListener{
+            try {
+                val input1 = editText1.text.toString().toInt()
+                val input2 = editText2.text.toString().toInt()
+                val result = input1 * input2
+                val intent = Intent(this, CalculatorActivity::class.java)
+                intent.putExtra("RESULT", result.toString())
+                startActivity(intent)
+            } catch(e: NumberFormatException){
+                val error = Toast.makeText(this, "Please Enter Valid Integers", Toast.LENGTH_SHORT)
+                error.show()
+            }
+        }
+
+        val divisionButton= findViewById<Button>(R.id.divisionButton)
+        divisionButton.setOnClickListener{
+            try {
+                val input1 = editText1.text.toString().toInt()
+                val input2 = editText2.text.toString().toInt()
+                val result = input1 / input2
+                val intent = Intent(this, CalculatorActivity::class.java)
+                intent.putExtra("RESULT", result.toString())
+                startActivity(intent)
+            } catch(e: NumberFormatException){
+                val error = Toast.makeText(this, "Please Enter Valid Integers", Toast.LENGTH_SHORT)
+                error.show()
+            } catch(e: ArithmeticException){
+                val error = Toast.makeText(this, "Error: $e", Toast.LENGTH_SHORT)
+                error.show()
+            }
         }
     }
 }
